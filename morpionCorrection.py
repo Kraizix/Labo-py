d0d8ddff3c34ec3245fcb2144081e24e4c04af2e
CReype <costa.reype@ynov.com>
2021-12-01 22:15:50 +0100

    comment modification

--- morpionCorrection.py
+++ morpionCorrection.py
@@ -1,7 +1,8 @@
 import random
 
+# déclaration de la grille de jeu
 grid=[]
-for i in range(9):
+for i in range(1,10):
     grid.append(i)
 
 #random
@@ -9,7 +10,7 @@ startPlayer=random.randint(1,2)
 
 Player=startPlayer
 
-
+# affichage déconseillé, mais utile pour comprendre l'autre technique
 def affBrut():
     print(grid[0], end="")
     print(grid[1], end="")
@@ -31,8 +32,11 @@ def affFor():
             print(grid[i+j], end="")
         print()
 
+# Gère un tour de jeu
 def play():
+    # global permet de récupérer une variable déclarée hors fonction et de pouvoir la modifier
     global Player
+    # Player%2+1 permet d'avoir le joueur actuel. Ex: si player =6: 6%2+1=1 -> donc au joueur 1 de jouer
     print("Joueur " + str(Player%2+1) + " à toi de jouer")
     signe=""
     #Determiner le signe X O
@@ -40,12 +44,15 @@ def play():
         signe="X"
     else : 
         signe = "O"
-
+    
+    #récupérer la grille
     global grid
+
+    # déclaration d'une liste pour accéder au chiffre sous des chaînes de caractère:
+    # ainsi on peut vérifier si l'input correspond à un chiffre pour pouvoir le transformer
     answer=[]
     for i in range(9):
         answer.append(str(i+1))
-    print(answer, end="")
     
     # si l'input est pas bon
     while True:
@@ -61,39 +68,50 @@ def play():
             print("Cette case est occupée")
             continue
         break
-    
+    # modification de la valeur de grid à la position case-1
     grid[case-1]=signe
 
     #Change de joueur
     Player=Player+1
 
+# vérifie si la partie doit se finir
 def verif():
     # Win?
     #horizontal
+    # nous donne les index de début de ligne
     iList = [0,3,6]
     for i in iList:
+        # verifie si la ligne n'a que le même signe
         if grid[i]==grid[i+1]==grid[i+2]:
             print("Joueur " + str(Player%2+1) + "a gagné!")
             return True
     # Vertical
+    # nous donne les index de début de colonne
     iList = [0,1,2]
     for i in iList:
+        # verifie si la colonne n'a que le même symbole
         if grid[i]==grid[i+3]==grid[i+6]:
-            
             print("Joueur " + str(Player%2+1) + "a gagné!")
             return True
     # Diago
+    # on vérifie les seuls diagonales possibles
+    # A noter que le "or" correspond un "ou" logique (soit une condition soit l'autre)
     if grid[2]==grid[4]==grid[6] or grid[0]==grid[4]==grid[8]:
-        
         print("Joueur " + str(Player%2+1) + " a gagné!")
         return True
     # Rempli?
+    # si les vérifications du dessus n'ont rien données, on vérifie si la grille est pleine
     if Player==startPlayer+10:
         print("Egalité!")
         return True
 
+    # si aucune des conditions n'est valide, on return false
+    # cela signifie que le jeu n'est pas finit.
     return False
 
+# boucle dite "main" qui va exécuter les autres fonctions tant que le jeu n'est pas finit (tant que verif() retourne un false)
+
+affBrut()
 while verif()==False:
     play()
-    affBrut()
+    affFor()
